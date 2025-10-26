92a0b08999283eab37cf92063b21636cf192f800
Thiago Rodrigues <thiago.nascimento@opah.com.br>
2025-10-26 14:31:58 -0300

    Adicionando configurações para deploy no git hub pages

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'roteirizador-web' title`, () => {
+  it(`should have the 'cond-notify-web' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('roteirizador-web');
+    expect(app.title).toEqual('cond-notify-web');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, roteirizador-web');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, cond-notify-web');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'roteirizador-web';
+  title = 'cond-notify-web';
 }

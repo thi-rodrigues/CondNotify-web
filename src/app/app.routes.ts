3b356b77ec6df228b20d589f94757be3e72bf797
Thiago Rodrigues <thiago.nascimento@opah.com.br>
2025-10-29 20:59:49 -0300

    Rotas não utilizadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,14 +10,14 @@ export const routes: Routes = [
       ),
     canActivate: []
   },
-  {
-    path: 'https://thi-rodrigues.github.io/cond-notify-web/',
-    loadComponent: () =>
-      import('./core/pages/login/login.component').then(
-        (c) => c.LoginComponent
-      ),
-    canActivate: []
-  },
+  // {
+  //   path: 'https://thi-rodrigues.github.io/cond-notify-web/',
+  //   loadComponent: () =>
+  //     import('./core/pages/login/login.component').then(
+  //       (c) => c.LoginComponent
+  //     ),
+  //   canActivate: []
+  // },
   {
     path: '/home',
     loadComponent: () =>
@@ -26,14 +26,14 @@ export const routes: Routes = [
       ),
     canActivate: [],
   },
-  {
-    path: 'https://thi-rodrigues.github.io/cond-notify-web/home',
-    loadComponent: () =>
-      import('./core/pages/home/home.component').then(
-        (c) => c.HomeComponent
-      ),
-    canActivate: []
-  },
+  // {
+  //   path: 'https://thi-rodrigues.github.io/cond-notify-web/home',
+  //   loadComponent: () =>
+  //     import('./core/pages/home/home.component').then(
+  //       (c) => c.HomeComponent
+  //     ),
+  //   canActivate: []
+  // },
   {
     path: '/order-details/:id',
     loadComponent: () =>

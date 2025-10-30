cb22822c03b06e54a9d6fd8c52113f981a610b1b
Thiago Rodrigues <thiago.nascimento@opah.com.br>
2025-10-29 21:28:03 -0300

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { OrderDetailsComponent } from './core/pages/order-details/order-details.component';
 
 export const routes: Routes = [
   {
@@ -10,32 +9,16 @@ export const routes: Routes = [
       ),
     canActivate: []
   },
-  // {
-  //   path: 'https://thi-rodrigues.github.io/cond-notify-web/',
-  //   loadComponent: () =>
-  //     import('./core/pages/login/login.component').then(
-  //       (c) => c.LoginComponent
-  //     ),
-  //   canActivate: []
-  // },
   {
-    path: '/home',
+    path: 'home',
     loadComponent: () =>
       import('./core/pages/home/home.component').then(
         (c) => c.HomeComponent
       ),
     canActivate: [],
   },
-  // {
-  //   path: 'https://thi-rodrigues.github.io/cond-notify-web/home',
-  //   loadComponent: () =>
-  //     import('./core/pages/home/home.component').then(
-  //       (c) => c.HomeComponent
-  //     ),
-  //   canActivate: []
-  // },
   {
-    path: '/order-details/:id',
+    path: 'order-details/:id',
     loadComponent: () =>
       import('./core/pages/order-details/order-details.component').then(
         (c) => c.OrderDetailsComponent
